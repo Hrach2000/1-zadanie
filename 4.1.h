02d71e9b67b8912e610f55c544528340fb4a78e0
Hrach2000 <33207142+Hrach2000@users.noreply.github.com>
2017-12-18 20:37:26 +0300

    Update 4.1.h

--- 4.1.h
+++ 4.1.h
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n;
-int uj[100002],qash[100002];
-pair <int,int> p[100002];
-int main(){
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>qash[i]>>uj[i];
-        p[i]=make_pair(uj[i],qash[i]);
+
+pair <int, int> p[100002];
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> p[i].second >> p[i].first ;
     }
-    sort(p+1,p+n+1);
-    for(int i=1;i<=n;i++){
-        qash[i]=p[i].second;
-        uj[i]=p[i].first;
-    }
-    int hqash=0,ans=0;
-    for(int i=1;i<=n;i++){
-        if(uj[i]>=hqash){
-            hqash+=qash[i];
+    sort(p + 1, p + n + 1);
+    int hqash = 0, ans = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        if(p[i].first >= hqash)
+        {
+            hqash += p[i].second;
             ans++;
         }
     }
-    cout<<ans<<endl;
+    cout << ans << endl;
     return 0;
 }
