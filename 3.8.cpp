ee936a126def1b8ef0bd0ceb0f8c6ee3192bd988
Hrach2000 <33207142+Hrach2000@users.noreply.github.com>
2017-12-19 00:37:08 +0300

    Update 3.8.cpp

--- 3.8.cpp
+++ 3.8.cpp
@@ -1,29 +1,40 @@
 #include <cmath>
 #include <iostream>
+#include <cstdio>
+#include <iomanip>
 using namespace std;
-const float e = 1e-10;
-float form(float x)
+
+double func(double x)
 {
-    return tan(x) - x;
-}
-float rek(float d, float y)
-{
-    float z = (d + y) / 2;    
-    if(form(d) * form(z) < 0) 
-        y = z;
-    else 
-        d = z;
-    if(fabs(y - d) > e && form(z) != 0)
-    {
-        return rek(d, y);
-    }
-    else 
-        return -1;
+    return tan(x)-x;
 }
 int main()
 {
-    float a, b;
+
+    const double eps = 0.0000000001;
+    double a, b, t, f1, f2, f3, x = 0;
     cin >> a >> b;
-    cout << rek(a, b);
+    if(func(a) * func(b) <= 0 )
+    {
+        do
+        {
+            t = ( a + b ) / 2;
+            f1 = func( a );
+            f2 = func( b );
+            f3 = func( t );
+            if( f1 * f3 <= 0)
+                b = t;
+
+            if( f2 * f3 <= 0 )
+                a = t;
+            x++;
+        }
+        while(fabs(f3) >= eps);
+        cout << setprecision(12) << t << " " << x;
+    }
+    else
+        cout << -1;
+
+
     return 0;
 }
